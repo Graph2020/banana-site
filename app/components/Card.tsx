6ba1432c6850622fea253f935b1991127cda8c2d
Jacopo <163671349+Graph2020@users.noreply.github.com>
2026-03-25 18:48:25 +0200

    add banana cards

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -7,6 +7,9 @@ interface CardProps {
   title: string;
   description: string;
   price: number;
+  spanLabel: string;
+  spanBg: string;
+  spanTextColor: string;
 }
 
 export default function Card({
@@ -14,16 +17,33 @@ export default function Card({
   title,
   description,
   price,
+  spanLabel,
+  spanBg,
+  spanTextColor,
 }: CardProps): JSX.Element {
   return (
-    <div className="font-body gap-elements flex h-96 w-full flex-col rounded-2xl bg-white p-4">
-      <div className="h-20 w-full overflow-hidden rounded-2xl">
-        <Image fill={true} src={imageSrc} alt={title} />
+    <div className="font-body gap-elements group relative flex h-96 w-full flex-col rounded-2xl bg-white p-4 transition-transform duration-300 hover:-translate-y-2 md:w-72">
+      <div className="relative h-48 w-full overflow-hidden rounded-2xl">
+        <Image
+          fill={true}
+          src={imageSrc}
+          alt={title}
+          className="object-cover transition-transform duration-300 group-hover:scale-105"
+        />
+        <span
+          className={`absolute top-2 right-2 rounded-full px-3 py-1 text-white`}
+          style={{ backgroundColor: spanBg, color: spanTextColor }}
+        >
+          {spanLabel}
+        </span>
       </div>
-      <h2 className="font-headline font-medium">{title}</h2>
-      <p className="mb-1">{description}</p>
+      <div>
+        <h2 className="font-headline mb-1 text-xl font-bold">{title}</h2>
+        <p>{description}</p>
+      </div>
+
       <div className="flex w-full items-center justify-between">
-        <p>${price.toFixed(2)}</p>
+        <p className="text-tertiary text-2xl font-black">${price.toFixed(2)}</p>
         <ButtonPrice />
       </div>
     </div>
--- app/sections/About.tsx
+++ app/sections/About.tsx
@@ -3,9 +3,10 @@ import clsx from "clsx";
 import React from "react";
 import { FaArrowRight } from "react-icons/fa";
 import { FaArrowLeft } from "react-icons/fa";
+import Card from "../components/Card";
 export default function About(): JSX.Element {
   return (
-    <section className="space-sections mt-20 flex min-h-screen w-full flex-col gap-2 rounded-tl-2xl rounded-tr-2xl bg-[#f4f9db] pt-20 md:mt-0">
+    <section className="space-sections mt-20 flex min-h-screen w-full flex-col gap-2 rounded-tl-2xl rounded-tr-2xl bg-[#edf1d7] pt-20 md:mt-0">
       <div className="gap-elements flex w-full flex-col sm:flex-row sm:items-center sm:justify-around md:justify-between">
         <div className="space-y-4">
           <h1 className="text-title text-secondary">About page</h1>
@@ -19,6 +20,36 @@ export default function About(): JSX.Element {
           <ButtonArrow icon={<FaArrowLeft />} isActive={false} />
           <ButtonArrow icon={<FaArrowRight />} isActive={true} />
         </div>
+      </div>
+      {/* Banana cards */}
+      <div className="mt-10 flex w-full flex-col gap-8 md:flex-row">
+        <Card
+          spanTextColor="black"
+          title="Banana One"
+          description="This is incredible banana is yours don't thank me"
+          spanLabel="fresh"
+          spanBg="#fffde7"
+          price={20}
+          imageSrc="/images/bananas.png"
+        />
+        <Card
+          spanTextColor="#fffde7"
+          title="Many Banans!"
+          description="Чорт забирай стільки бананів ну це треба брати інакше розгребуть"
+          spanLabel="quantity"
+          spanBg="#00a86b"
+          price={25}
+          imageSrc="/images/manyBananas.png"
+        />
+        <Card
+          spanTextColor="orange"
+          title="Protein Banana"
+          description="Чувак в щоці від банана, як ще це можна описати я не зроз"
+          spanLabel="power"
+          spanBg="#e2e6b4"
+          price={30}
+          imageSrc="/images/memeBanana.png"
+        />
       </div>
     </section>
   );
